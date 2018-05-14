487d81453d17fbf7560152b93c930325662e7da5
Coding Labs <codinglabs.wavelabs@gmail.com>
2018-05-14 14:33:58 +0530

    Coding Labs

---
+++ app/src/main/java/com/example/android/widgets/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.widgets;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
